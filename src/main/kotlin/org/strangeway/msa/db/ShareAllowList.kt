577ef61fde69145e990d433ffb0d8db417302d87
Yuriy Artamonov <jreznot@yandex.ru>
2022-01-02 19:32:47 +0300

    Include popular libraries to allow list

--- src/main/kotlin/org/strangeway/msa/db/ShareAllowList.kt
+++ src/main/kotlin/org/strangeway/msa/db/ShareAllowList.kt
@@ -1,17 +1,29 @@
 package org.strangeway.msa.db
 
 internal val SHARE_JAVA_PACKAGE_PREFIXES = listOf(
-  "com.intellij.util",
-  "io.grpc",
+  "com.intellij.util.",
+  "io.grpc.",
+  "io.javalin.",
+  "io.ktor.",
   "io.micronaut.",
-  "io.netty",
+  "io.netty.",
   "io.quarkus.",
+  "io.restassured",
   "io.smallrye.",
-  "io.vertx",
+  "io.vertx.",
   "jakarta.",
   "java.",
   "javax.",
+  "jdbi.",
+  "kong.unirest.",
+  "kotlinx.",
+  "okhttp3.",
   "org.apache.",
   "org.eclipse.",
-  "org.springframework."
+  "org.hibernate.",
+  "org.http4k.",
+  "org.jooby.",
+  "org.jooq.",
+  "org.springframework.",
+  "retrofit2."
 )
